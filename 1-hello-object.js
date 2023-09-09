2bd16c0d43afbc2978f7e5d690bb7d62a6eb4441
BigFatDuck1 <clementho999@gmail.com>
2023-09-10 00:50:39 +0800

    1 Hello Object completed

---
+++ 1-hello-object.js
@@ -0,0 +1,15 @@
+
+//Create empty object user, add property value
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+// console.log(user);
+
+//Change name to Pete
+user.name = "Pete";
+
+//Delete name
+delete user.name;
+
+//Check output
+console.log(user)
