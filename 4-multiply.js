8b3f56c7abdcfee0e1d502bf1c2786e27a4e1f0b
Clement <clementho999@gmail.com>
2023-09-11 01:42:41 +0800

    Created file for task 4

---
+++ 4-multiply.js
@@ -0,0 +1,3 @@
+//Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+//Leaves values that are of string type unmodified
+//Use typeof to check for "number"
