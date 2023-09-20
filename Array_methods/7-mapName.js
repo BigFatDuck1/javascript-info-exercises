97ec56f719c66956c68c65f9aac40be6d012aa1f
BigFatDuck1 <clementho999@gmail.com>
2023-09-20 18:44:33 +0800

    Added mapName()

--- Array_methods/7-mapName.js
+++ Array_methods/7-mapName.js
@@ -1,6 +1,25 @@
 //You have an array of user objects, each one has user.name. 
 //Write the code that converts it into an array of names.
 
-let mapName = () => {
+let mapName = (users_array) => {
+
+    let result = [];
+
+    result = users_array.map((element) => {
+        //todo
+    })
     
 }
+
+//Test
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = [];
+
+names = mapName(users);
+
+console.log(names);
