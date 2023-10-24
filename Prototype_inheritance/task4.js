ada79fd404246b3e1d31daad34bfeee6441d108b
BigFatDuck1 <clementho999@gmail.com>
2023-10-24 16:19:50 +0800

    Completed task4.js

---
+++ Prototype_inheritance/task4.js
@@ -0,0 +1,38 @@
+// We have two hamsters: speedy and lazy inheriting from the general hamster object.
+
+// When we feed one of them, the other one is also full. Why? How can we fix it?
+
+let hamster = {
+    stomach: [],
+  
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+  
+let speedy = {
+    stomach: [], //Add this line, otherwise it will be a purely inherited property
+    __proto__: hamster
+};
+  
+let lazy = {
+    stomach: [], //Add this line, otherwise it will be a purely inherited property
+    __proto__: hamster
+};
+  
+// This one found the food
+speedy.eat("apple");
+console.log( speedy.stomach ); // apple
+
+// This one also has it, why? fix please.
+console.log( lazy.stomach ); // apple
+
+//Method 2 
+
+let alternate_hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach = [food];
+    }
+}
